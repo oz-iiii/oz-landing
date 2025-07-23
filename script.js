c2f01407939fcc2f3f22806650ce15e59dc3c5bb
a <a@aui-MacBookPro.local>
2025-07-23 10:31:24 +0900

    정리

---
+++ script.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".swiper-container", {
+  centeredSlides: true,
+  slidesPerView: 1.1,
+  centeredSlides: true,
+  spaceBetween: 16 /* 원래 16 */,
+  loop: true,
+  grabCursor: true,
+});
+/*수정*/
--- test/script.js
+++
@@ -1,8 +0,0 @@
-const swiper = new Swiper(".swiper-container", {
-	centeredSlides: true,
-	slidesPerView: 1.1,
-	centeredSlides: true,
-	spaceBetween: 16 /* 원래 16 */,
-	loop: true,
-	grabCursor: true,
-});
